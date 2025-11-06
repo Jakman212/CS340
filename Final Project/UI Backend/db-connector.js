f5529761df6a234f13750d8714b1afd31a7d67bc
Jakman212 <jeremy.dempsey1990@gmail.com>
2025-11-06 06:18:27 +0000

    Commit 2

---
+++ Final Project/UI Backend/db-connector.js
@@ -0,0 +1,17 @@
+// database/db-connector.js
+// Database connection configuration
+
+var mysql = require('mysql');
+
+// Create connection pool
+// TODO: Update these values with your actual database credentials
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_YOUR_ONID',           // CHANGE THIS
+    password        : 'YOUR_DATABASE_PASSWORD',     // CHANGE THIS
+    database        : 'cs340_YOUR_ONID'            // CHANGE THIS
+});
+
+// Export the pool
+module.exports.pool = pool;
